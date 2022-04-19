f26c48044f12bbf0e1a59cd7447fa3a24ab9c0ea
bartosz-skejcik <bartek@paczesny.pl>
2022-04-19 21:17:43 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Next.js</title>
+            </Head>
+            <div>
+                <h1>Next.js</h1>
+                <p>
+                    Next.js is a minimalistic framework for server-side rendering.
+                </p>
+            </div>
+        </>
+    )
+}
